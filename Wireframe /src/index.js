bd591f3c049c38de4a6d71c93c397da07f5db7ba
olehivoniuk <olehivoniuk767@gmal.com>
2022-10-11 11:25:26 +0300

    day fixed

--- Wireframe /src/index.js
+++ Wireframe /src/index.js
@@ -4,6 +4,6 @@ let hours = now.getHours();
 
 let minutes = now.getMinutes();
 let days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let day = days[now.getDate()];
+let day = days[now.getDay()];
 let lineDate = document.querySelector("#currentTime")
 lineDate.innerHTML = day + " " + hours + ":" + minutes;
