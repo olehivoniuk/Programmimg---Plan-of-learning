fea1f5f3f117dc6070f5d8643ef615074ca1fb83
olehivoniuk <olehivoniuk767@gmal.com>
2022-10-11 11:23:50 +0300

    day

--- Wireframe /src/index.js
+++ Wireframe /src/index.js
@@ -3,7 +3,7 @@ console.log(now.getDate())
 let hours = now.getHours();
 
 let minutes = now.getMinutes();
-let days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"]
 let day = days[now.getDate()];
-let lineDate = document.querySelector("#currentTime");
+let lineDate = document.querySelector("#currentTime")
 lineDate.innerHTML = day + " " + hours + ":" + minutes;
